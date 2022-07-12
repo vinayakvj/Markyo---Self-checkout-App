afa8be6ce9dd9ffc6ed82275c1f49176b020c744
vinayak <vinayakjay12@gmail.com>
2022-07-12 20:15:16 +0530

    updated env

--- Server/config/dbconfig.js
+++ Server/config/dbconfig.js
@@ -1,5 +1,8 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 module.exports = {
   secret: "yoursecret",
-  database:
-    "mongodb+srv://vinayak:qwerty1234@cluster0.3ktlj.mongodb.net/?retryWrites=true&w=majority",
+  database: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3ktlj.mongodb.net/?retryWrites=true&w=majority`,
 };
