cf9ed09d1988cdb4ed414baae9ae26a173bf91d4
VigneshSKumar <92641241+VigneshSKumar@users.noreply.github.com>
2022-08-03 13:43:11 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,6 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyThemes {
-  static final darkTheme = ThemeData(
-    scaffoldBackgroundColor: Colors.grey.shade900,
-  );
-
-  static final lightTheme = ThemeData(
-    scaffoldBackgroundColor: Colors.white,
-  );
-}
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -21,19 +11,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.system,
-      theme: MyThemes.lightTheme,
-      darkTheme: MyThemes.darkTheme,
       home: Scaffold(
         appBar: AppBar(
           leading: IconButton(
             icon: Icon(
-              Icons.menu_outlined,
+              Icons.home,
               color: Colors.black,
             ),
             onPressed: () {},
           ),
-          // add more IconButton
           centerTitle: true,
           backgroundColor: Color.fromARGB(121, 39, 187, 29),
           title: Text(
@@ -44,51 +30,13 @@ class MyApp extends StatelessWidget {
               color: Color.fromARGB(255, 250, 246, 246),
             ),
           ),
-
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.person_outlined),
-              color: Colors.black,
-              onPressed: () {},
-            ),
-          ],
         ),
         backgroundColor: Colors.grey.shade900,
-        body: ListView(
-          children: [
-            Text(
-              "SHOP AND PAY BILL ON YOUR PHONE!",
+        body: Center(
+          child: Text("THANK YOU FOR THE PURCHASE :)",
               textAlign: TextAlign.center,
               style: TextStyle(
-                fontSize: 30,
-                fontFamily: "Poppins",
-                color: Colors.white,
-              ),
-            ),
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  SizedBox(height: 100),
-                  IconButton(
-                    onPressed: () {},
-                    color: Colors.white,
-                    splashColor: Color.fromARGB(121, 39, 187, 29),
-                    icon: Icon(Icons.qr_code_scanner),
-                    iconSize: 28,
-                  ),
-                  SizedBox(height: 100),
-                  IconButton(
-                    onPressed: () {},
-                    color: Colors.white,
-                    splashColor: Color.fromARGB(121, 39, 187, 29),
-                    icon: Icon(Icons.shopping_cart_outlined),
-                    iconSize: 28,
-                  ),
-                ],
-              ),
-            ),
-          ],
+                  fontSize: 30, fontFamily: "Poppins", color: Colors.white)),
         ),
       ),
     );
