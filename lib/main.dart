0a90f43ab3fd1569b7800d077c1a93a5592f400b
VigneshSKumar <92641241+VigneshSKumar@users.noreply.github.com>
2022-08-03 12:53:05 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,165 +1,96 @@
 import 'package:flutter/material.dart';
-import 'pages/home.dart'; 
-import 'pages/login.dart';
-import './pages/splash.dart';
-import './pages/signup.dart';
 
-void main() => runApp(MaterialApp(
-    home: splash()
-    ),
-);
+void main() {
+  runApp(const MyApp());
+}
 
-class Login extends StatelessWidget {
-  
-  const Login({Key? key}) : super(key: key);
+class MyThemes {
+  static final darkTheme = ThemeData(
+    scaffoldBackgroundColor: Colors.grey.shade900,
+  );
+
+  static final lightTheme = ThemeData(
+    scaffoldBackgroundColor: Colors.white,
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Markyo',
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      theme: MyThemes.lightTheme,
+      darkTheme: MyThemes.darkTheme,
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(
+              Icons.menu_outlined,
+              color: Colors.black,
+            ),
+            onPressed: () {},
+          ),
+          // add more IconButton
+          centerTitle: true,
+          backgroundColor: Color.fromARGB(121, 39, 187, 29),
+          title: Text(
+            'MARKYO',
+            style: TextStyle(
+              fontSize: 22.0,
+              fontFamily: "Poppins",
+              color: Color.fromARGB(255, 250, 246, 246),
+            ),
+          ),
+
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.person_outlined),
+              color: Colors.black,
+              onPressed: () {},
+            ),
+          ],
         ),
-      ),
-        backgroundColor: Color.fromARGB(255, 158, 1, 185),
-        centerTitle: true,
-      ),
-      body: Center(
-          child: Container(
-            padding: EdgeInsets.only(left: 35,right: 35),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('LoginD;',
-                style: TextStyle(
-                  fontSize: 30.0
-                ),
+        backgroundColor: Colors.grey.shade900,
+        body: ListView(
+          children: [
+            Text(
+              "SHOP AND PAY BILL ON YOUR PHONE!",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 30,
+                fontFamily: "Poppins",
+                color: Colors.white,
               ),
-                Container(
-                  padding: EdgeInsets.only(top: 20.0,bottom: 20.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Username'
-                    ),
-                  ),
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Password'
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.only(top: 40.0),
-                  child: RaisedButton(
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(height: 100),
+                  IconButton(
                     onPressed: () {},
-                    child: Text('Login'),
+                    color: Colors.white,
+                    splashColor: Color.fromARGB(121, 39, 187, 29),
+                    icon: Icon(Icons.qr_code_scanner),
+                    iconSize: 28,
                   ),
-                ),
-              ],
+                  SizedBox(height: 100),
+                  IconButton(
+                    onPressed: () {},
+                    color: Colors.white,
+                    splashColor: Color.fromARGB(121, 39, 187, 29),
+                    icon: Icon(Icons.shopping_cart_outlined),
+                    iconSize: 28,
+                  ),
+                ],
+              ),
             ),
-          )
-        ), 
-      floatingActionButton: FloatingActionButton(
-        child: Text('click'),
-        onPressed: () {},
-        backgroundColor: Color.fromARGB(255, 158, 1, 185),
-      ),
-      );
-  }
-}
-
-
-// state full widget so changes can be made.
-
-/* class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
-
-  @override
-  State<LoginPage> createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-
-  final _textController1 = TextEditingController();
-  final _textController2 = TextEditingController();
-
-    String firstName = '';
-    String secondName = '';
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Markyo',
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
+          ],
         ),
       ),
-        backgroundColor: Color.fromARGB(255, 158, 1, 185),
-        centerTitle: true,
-      ),
-      body: Center(
-          child: Container(
-            padding: EdgeInsets.only(left: 35,right: 35),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Hello, $firstName $secondName' ,
-                style: TextStyle(
-                  fontSize: 30.0
-                ),
-              ),
-                Container(
-                  padding: EdgeInsets.only(top: 20.0,bottom: 20.0),
-                  child: TextField(
-                    controller: _textController1,
-                    decoration: InputDecoration(
-                      hintText: 'Username'
-                    ),
-                  ),
-                ),
-                TextField(
-                  controller: _textController2,
-                  decoration: InputDecoration(
-                    hintText: 'Password'
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.only(top: 40.0),
-                  child: RaisedButton(
-                    onPressed: () {
-                      setState(() {
-                        firstName = _textController1.text;
-                        secondName = _textController2.text;
-                      });
-                    },
-                    child: Text('Login'),
-                  ),
-                ),
-              ],
-            ),
-          )
-        ), 
-      floatingActionButton: FloatingActionButton(
-        child: Text('clear'),
-        onPressed: () {
-          setState(() {
-            firstName = '';
-            secondName = '';
-
-             Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) {
-              return Home();
-            }),); 
-          });
-        },
-        backgroundColor: Color.fromARGB(255, 158, 1, 185),
-      ),
-      );
+    );
   }
-} */
+}
