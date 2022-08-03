62abe081802105329265cf82d0ed4c9cdf3da76d
vinayak <vinayakjay12@gmail.com>
2022-08-03 15:56:10 +0530

    qrcode

--- lib/pages/qrcode.dart
+++ lib/pages/qrcode.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class qrcode extends StatefulWidget {
   final value;
@@ -31,7 +32,23 @@ class _qrcodeState extends State<qrcode> {
         child: Container(
           child: Column(
             children: [
-              Text('${widget.value}')
+              Padding(
+                padding: const EdgeInsets.only(top: 100.0),
+                child: QrImage(
+                  data:  widget.value.toString(),
+                  size: 200,
+
+                ),
+              ),
+              Text('Total amt: ${widget.value}'),
+              Padding(
+                padding: const EdgeInsets.only(top: 20.0),
+                child: Text("SCAN TO PAY.",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold
+                ),),
+              )
             ],
           ),
         ),)
